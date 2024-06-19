d45ec7fe3c80865da0fa40743dce29bfdbe3f52a
Черкашин Иван Алексеевич <cherkashin_ia@sibsiu.ru>
2024-06-19 09:18:55 +0700

    Small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, {FC} from 'react';
+
+import {
+  LoginPage, CreateTablePage,
+  CreateUserPage
+} from '~/components/pages';
+
 import './App.css';
-import LoginPage from './components/pages/login';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CreateTablePage from './components/pages/createTable';
-import CreateUserPage from './components/pages/createUser';
 
 const App:FC = () =>{
   return (
     <div>
-      <CreateUserPage />
+      <LoginPage />
     </div>
   );
 }
